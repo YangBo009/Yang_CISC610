c7c57f742532c96fbe89947dac0f89790ff407ab
YangBo009 <BOYang@my.harrisburgu.edu>
2017-04-26 22:39:33 -0400

    Add files via upload

--- assignment 4.py
+++ assignment 4.py
@@ -1,45 +1,52 @@
-import urllib
-
+class Empty(Exception):
+    pass
 class ArrayStack:
-    def ___init__(self):
-        self._data =[]
+    def __init__(self):
+        self._data = []
+
     def __len__(self):
         return len(self._data)
-    def is_empty(self):
-        return len(self. _data)==0
-    def push(self,e):
-        self._data.append(e)
+
+    def push(self, item):
+        self._data.append(item)
+
+    def isEmpty(self):
+        return (self._data == [])
+
     def top(self):
-        if self.is_empty():
+        if self.isEmpty():
             raise Empty('Stack is empty')
-        return self._data[-1]
+            return self._data[-1]
     def pop(self):
-        if self.is_empty():
+        if self.isEmpty():
             raise Empty('Stack is empty')
         return self._data.pop()
 
 
-
 def is_matched_html(raw):
-    S = ArrayStack
+    s = ArrayStack()
     j = raw.find("<")
-    while j!=-1:
+    while j != -1:
         k = raw.find('>',j+1)
         if k == -1:
             return False
         tag = raw[j+1:k]
         if not tag.startswith('/'):
-            S.push(tag)
+            s.push(tag)
         else:
-            if S.is_empty():
+            if s.isEmpty():
                 return False
-            if tag[1:]!=S.pop():
+            if tag[1:]!=s.pop():
                 return False
         j = raw.find('<',k+1)
-    return S.is_empty()
+    return s.isEmpty()
 
-link = "http://www.gutenberg.org/files/54560/54560-h/54560-h.htm"
-html = urllib.urlopen(link)
-raw = html.read()
+Hstr = '<center><h1> Let us to work hard </h1> </center>'
+if is_matched_html(Hstr)== False:
+    print "False"
+else:
+    print "True"
+
+
 
 
